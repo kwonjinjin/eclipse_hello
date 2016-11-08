f1cbf93ca81bf64137442d238dd2b7a163e8473c
kwonjinjin <kwonjin2952@naver.com>
2016-11-08 14:39:21 +0900

    PushPrint modifed on GItHub

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -5,6 +5,7 @@ public class PushPrint {
 		// TODO Auto-generated method stub
 
 		System.out.println("Add Java Class File Push");
+		System.out.println("world with 365");                					
 	}
-
+	
 }
